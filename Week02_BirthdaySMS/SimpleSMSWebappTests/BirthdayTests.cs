c25efc8289714d6d561f9b5ba8670e9611b68a6f
Julia Lin <linjulia@usc.edu>
2018-06-05 14:13:06 -0700

    Add 7 tests

---
+++ Week02_BirthdaySMS/SimpleSMSWebappTests/BirthdayTests.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+namespace ProjectBirthday.Tests
+{
+    [TestClass]
+    public class BirthdayTests
+    {
+        const string expectedNormal = @"Your Age: \d+.\d+ years old.\nThere are \d+ days until your next birthday! ";
+
+        [TestMethod]
+        public void Normal_Birthday_Later_In_The_Year_Test()
+        {
+            // Arrange
+            string input = "12/31/1996";
+            string expected = expectedNormal;
+            // Act
+            string actual = Birthday.ProcessRequest(input);
+            // Assert
+            Assert.IsTrue(Regex.Match(actual, expected).Success);
+        }
+
+        [TestMethod]
+        public void Normal_Birthday_Earlier_In_The_Year_Test()
+        {
+            // Arrange
+            string input = "01/01/1996";
+            string expected = expectedNormal;
+            // Act
+            string actual = Birthday.ProcessRequest(input);
+            // Assert
+            Assert.IsTrue(Regex.Match(actual, expected).Success);
+        }
+
+        [TestMethod]
+        public void Birthday_Is_Today_Test()
+        {
+            // Arrange
+            var date = System.DateTime.Today;
+            date = date.AddYears(-22);
+            string input = date.ToString("MM/dd/yyyy");
+            string expected = expectedNormal + "Happy birthday!";
+            // Act
+            string actual = Birthday.ProcessRequest(input);
+            // Assert
+            Assert.IsTrue(Regex.Match(actual, expected).Success);
+        }
+
+        [TestMethod]
+        public void Too_Old_Birthday_Test()
+        {
+            // Arrange
+            string input = "01/01/1850";
+            string expected = "You shouldn't be older than the oldest person ever lived. Please enter a valid birthday.";
+            // Act
+            string actual = Birthday.ProcessRequest(input);
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Birthday_In_The_Future_Test()
+        {
+            // Arrange
+            string input = "01/01/2028";
+            string expected = "The birthday you've entered is in the future! Please enter a valid birthday.";
+            // Act
+            string actual = Birthday.ProcessRequest(input);
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Incorrect_Date_Format_Test()
+        {
+            // Arrange
+            string input = "436841";
+            string expected = "You've entered an incorrect date format. Please enter the date in mm/dd/yyyy.";
+            // Act
+            string actual = Birthday.ProcessRequest(input);
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Incorrect_Date_Format_Test_1()
+        {
+            // Arrange
+            string input = "asdf";
+            string expected = "You've entered an incorrect date format. Please enter the date in mm/dd/yyyy.";
+            // Act
+            string actual = Birthday.ProcessRequest(input);
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Week02_BirthdaySMS/SimpleSMSWebappTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleSMSWebappTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleSMSWebappTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("88ba31ff-e6f3-4f4e-a774-8f796dffcdea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
